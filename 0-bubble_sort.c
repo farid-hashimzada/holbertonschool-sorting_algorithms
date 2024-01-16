6e8b080b4f551aa2dfa239fba347dc373ae8167b
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-01-16 12:13:25 -0800

    bubble sort solution

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/*bubble_sort - a function that sorts an array using Bubble sort algorithms
+ * @array: point to array
+ * @size: size of array
+ * Return: void
+ * */
+
+
+void bubble_sort(int *array, size_t size)
+{
+int i = 0, temp;
+bool swapped = false;
+do
+{
+swapped = false;
+for (size_t j = 0; j < size - i - 1;j++)
+{
+if (array[j] >  array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+swapped = true;
+}
+}
+i++;
+print_array(array, size);
+}while(swapped);
+}
