17b6823a804c21b0312e4dfa2c6a633a243a0c54
Farid Hashimzade <124304902+farid-hashimzada@users.noreply.github.com>
2024-01-18 00:21:36 +0400

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -24,7 +24,8 @@ void selection_sort(int *array, size_t size)
 		if (min_i != i)
 		{
 			temp = array[min_i];
-			array[min_i] = array[i];
+			array[min_i] = array[I];
+			array[i] = temp;
 			print_array(array, size);
 		}
 	}
