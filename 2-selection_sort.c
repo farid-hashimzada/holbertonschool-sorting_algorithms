e81da54841317c0b53f085bb057adafe41a511ce
farid-hashimzada <hashimzadafarid@gmail.com>
2024-01-17 12:11:50 -0800

    2-selection_sort.c update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,12 +1,12 @@
 #include "sort.h"
 
-
 /**
  * selection_sort - sorts an arrray
- * @array: array  sorted
+ * @array: array sorted
  * @size: size array
  *
  */
+
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_i;
@@ -16,6 +16,11 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size; i++)
 	{
 		min_i = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_i])
+				min_i = j;
+		}
 		if (min_i != i)
 		{
 			temp = array[min_i];
